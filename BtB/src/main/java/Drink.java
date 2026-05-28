7a3f6296ce3da855ad2bca76922aca55bfd687a5
Elizaspr <spratt.eliza@gmail.com>
2026-05-28 19:00:13 -0400

    drinks class finished

--- BtB/src/main/java/Drink.java
+++ BtB/src/main/java/Drink.java
@@ -1,2 +1,41 @@
 public class Drink {
+  private String size;
+  private String flavor;
+
+  public Drink(String size, String flavor){
+      this.size = size;
+      this.flavor = flavor;
+  }
+
+  //getters
+  public String getSize() {
+      return size;
+  }
+
+  public String getFlavor() {
+      return flavor;
+  }
+
+  //setters
+  public void setSize(String size){
+      this.size = size;
+  }
+
+  public void setFlavor(String flavor) {
+      this.flavor = flavor;
+  }
+
+  public double calculatePrice() {
+      switch (size.toLowerCase()) {
+          case "small":
+              return 2.00;
+          case "medium":
+              return 2.50;
+          case "large":
+              return 3.00;
+          default:
+              return 0.0;
+      }
+  }
 }
+
--- BtB/src/main/java/UserInterface.java
+++ BtB/src/main/java/UserInterface.java
@@ -1,2 +1,4 @@
+import java.util.Scanner;
+
 public class UserInterface {
 }
