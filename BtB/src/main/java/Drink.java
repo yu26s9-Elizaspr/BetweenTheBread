81f0f90c4b318e78aa78ddb200d5fdf4f4de9158
Elizaspr <spratt.eliza@gmail.com>
2026-05-28 19:50:38 -0400

    added enums

--- BtB/src/main/java/Drink.java
+++ BtB/src/main/java/Drink.java
@@ -1,41 +1,29 @@
 public class Drink {
-  private String size;
-  private String flavor;
+    private String size;
+    private String flavor;
 
-  public Drink(String size, String flavor){
-      this.size = size;
-      this.flavor = flavor;
-  }
+    public Drink(String size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
 
-  //getters
-  public String getSize() {
-      return size;
-  }
+    //getters
+    public String getSize() {
+        return size;
+    }
 
-  public String getFlavor() {
-      return flavor;
-  }
+    public String getFlavor() {
+        return flavor;
+    }
 
-  //setters
-  public void setSize(String size){
-      this.size = size;
-  }
+    //setters
+    public void setSize(String size) {
+        this.size = size;
+    }
 
-  public void setFlavor(String flavor) {
-      this.flavor = flavor;
-  }
-
-  public double calculatePrice() {
-      switch (size.toLowerCase()) {
-          case "small":
-              return 2.00;
-          case "medium":
-              return 2.50;
-          case "large":
-              return 3.00;
-          default:
-              return 0.0;
-      }
-  }
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
 }
+
 
--- BtB/src/main/java/Topping.java
+++ BtB/src/main/java/Topping.java
@@ -1,2 +1,21 @@
-public class Topping {
+public abstract class Topping {
+    private String name;
+    private boolean extra;
+
+    public Topping (String name, boolean extra) {
+        this.name = name;
+        this.extra = extra;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isExtra() {
+        return extra;
+    }
+
+
+    public abstract double calculatePrice(String sandwichSize);
+
 }
