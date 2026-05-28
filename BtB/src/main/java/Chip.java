644d9bc454b18c567afcebadc87563d8905b6dc8
Elizaspr <spratt.eliza@gmail.com>
2026-05-28 19:09:31 -0400

    Chips class

--- BtB/src/main/java/Chip.java
+++ BtB/src/main/java/Chip.java
@@ -1,3 +1,20 @@
 public class Chip {
+    private String type;
 
+    public Chip (String type) {
+        this.type = type;
+    }
+
+    // getters
+    public String getType() {
+        return type;
+    }
+    //setters
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public double calculatePrice() {
+        return 1.50;
+    }
 }
